2dd2a23c146686a7d67ffd6335d6893bf426dd76
AnthonyLacrabere <anthony.lacra@gmail.com>
2019-09-17 12:47:28 +0200

    <Feature> Add input for top and bottom text

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -9,19 +9,48 @@ class MemeGenerator extends Component {
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
+        this.handleChange = this.handleChange.bind(this)
     }
     
     componentDidMount() {
         fetch("https://api.imgflip.com/get_memes")
         .then(response => response.json())
-        .then(data => this.setState({
-            allMemeImgs: data.memes
-        }))
+        .then(response => {
+            const { memes } = response.data 
+            
+            this.setState({
+            allMemeImgs: memes
+            })
+        })
+    }
+
+    handleChange(e) {
+        const {name, value} = e.target
+        this.setState({
+            [name] : value
+        })
     }
 
     render() {
         return (
-            <h1>MEME GENERATOR SECTION</h1>
+           <div>
+               <form className="meme-form">
+                    <input 
+                        name="topText"
+                        placeholder="text du haut"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                    />
+
+                    <input 
+                        name="bottomText"
+                        placeholder="text du bas"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                    />
+                    <button>Generate</button>
+               </form>
+           </div>
         )
     }
 }
